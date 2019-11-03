04ccef3b615538f49a1b6bdca9d77f6ef53c4a08
sheikhsuleman123 <mssheikh333@gmail.com>
2019-11-03 18:44:04 +0500

    header and navbar is ok

---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      
+//    Header 
+  <header className="masthead">
+    <div className="container">
+      <div className="intro-text">
+        <div className="intro-lead-in">Welcome To Our Studio!</div>
+        <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
+        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+      </div>
+    </div>
+  </header>
+        );
+  }
+}
